7a79d5228bdd35976dfed8b4befb2192a278c9fa
emmanukw <kidesh.kidera@gmail.com>
2023-04-04 09:51:31 +0300

    driver updated

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
-chrome_driver_path = r"C:\Users\Manu\Desktop\Development\chromedriver.exe"
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
-driver.get("https://www.udemy.com/?utm_source=adwords-brand&utm_medium=udemyads&utm_campaign=Brand-Udemy_la.EN_cc.ROW"
-           "&utm_term=_._ag_80315195513_._ad_535757779892_._de_c_._dm__._pl__._ti_kwd-296956216253_._li_9076844_"
-           "._pd__._&utm_term=_._pd__._kw_udemy_._&matchtype=b&gclid"
-           "=EAIaIQobChMIxLek_u79_QIVdpBoCR1YKwBqEAAYASAAEgJ5v_D_BwE")
+ser = Service(r"C:\Users\Manu\Desktop\Development\chromedriver.exe")
+op = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=ser, options=op)
+driver.get("https://www.python.org/events/python-events/")
 
-x = driver.find_element(by="css_selector", value="q")
+x = driver.find_elements(By.CSS_SELECTOR, "time")
+
+for time in x:
+    print(time.text)
+# event_name = driver.find_elements(by="css_selector", value=".event_widget a")
 
 driver.quit()
