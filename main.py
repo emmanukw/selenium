709dcf53e33c0a8cbb739ee38c4f879afd195193
emmanukw <kidesh.kidera@gmail.com>
2023-04-04 15:47:44 +0300

    driver updated

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ op = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=ser, options=op)
 driver.get("https://www.python.org/events/python-events/")
 
-x = driver.find_elements(By.CSS_SELECTOR, "time")
+list1 = []
+list2 = []
 
-for time in x:
-    print(time.text)
-# event_name = driver.find_elements(by="css_selector", value=".event_widget a")
+event_time = driver.find_elements(By.CSS_SELECTOR, "time")
+for i in event_time:
+    list1.append(i.text)
+
+event_name = driver.find_elements(By.CLASS_NAME, "event-title")
+for i in event_name:
+    list2.append(i.text)
+
+my_dict = {}
+for i in range(len(list1)):
+    my_dict[list1[i]] = list2[i]
+print(my_dict)
+
 
 driver.quit()
