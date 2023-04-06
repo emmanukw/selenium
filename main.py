f158265141e0fea27a141311f90bec212f7ec8c5
emmanukw <kidesh.kidera@gmail.com>
2023-04-06 11:11:55 +0300

    driver updated

--- main.py
+++ main.py
@@ -11,16 +11,11 @@ list1 = []
 list2 = []
 
 event_time = driver.find_elements(By.CSS_SELECTOR, "time")
-for i in event_time:
-    list1.append(i.text)
-
 event_name = driver.find_elements(By.CLASS_NAME, "event-title")
-for i in event_name:
-    list2.append(i.text)
 
 my_dict = {}
-for i in range(len(list1)):
-    my_dict[list1[i]] = list2[i]
+for i in range(len(event_time)):
+    my_dict[event_time[i].text] = event_name[i].text
 print(my_dict)
 
 
